0e78a1466217f621dd0c5cbf7c678e71c6543904
Heidi Snell <hms8694@gmail.com>
2022-05-13 09:57:28 -0500

    added generationTime range

--- conways-game-of-life/src/App.tsx
+++ conways-game-of-life/src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { Game } from "./Game";
 
 function App() {
+  let [generationTime, setGenerationTime] = useState(300);
+
+  const generationTimeOnChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setGenerationTime(parseInt(event.target.value));
+  };
+
   return (
     <div className="App">
-      <Game dimension={50} generationTime={300}></Game>
+      <Game dimension={30} generationTime={generationTime}></Game>
+      <label htmlFor=""></label>
+      <input
+        type="range"
+        name="generationTime"
+        id="generationTime"
+        min={0}
+        max={1000}
+        onChange={generationTimeOnChange}
+      />
     </div>
   );
 }
