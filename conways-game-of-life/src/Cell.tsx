ea44b13b5e5aed1f3a5d6f151d1910a19a04b5b5
Heidi Snell <hms8694@gmail.com>
2022-05-15 18:46:58 -0500

    update UI

--- conways-game-of-life/src/Cell.tsx
+++ conways-game-of-life/src/Cell.tsx
@@ -7,8 +7,8 @@ const Cell: React.FC<{ isAlive: boolean; toggleIsAlive: () => void }> = ({
   };
   return (
     <div onMouseOver={handleClick}>
-      <p className="bg-sky-900 text-xl text-center text-sky-300 h-8 w-8">
-        {isAlive ? "▇" : "-"}
+      <p className="bg-sky-900 text-2xl text-center text-sky-300 h-8 w-8">
+        {isAlive ? "▇" : " "}
       </p>
     </div>
   );
