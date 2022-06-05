f77da27b92d08cf931d6e5c460513fda58776c46
Heidi Snell <hms8694@gmail.com>
2022-06-04 21:05:28 -0400

    old edits

--- conways-game-of-life/src/App.tsx
+++ conways-game-of-life/src/App.tsx
@@ -2,26 +2,51 @@ import { useState } from "react";
 import { Game } from "./Game";
 
 function App() {
-  let [lifespan, setLifespan] = useState(200);
+  const initialLifespan = 200;
+  const initialDimension = 20;
+
+  let [lifespan, setLifespan] = useState(initialLifespan);
+  let [dimension, setDimension] = useState(initialDimension);
 
   const lifespanOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLifespan(parseInt(event.target.value));
   };
 
+  const dimensionOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDimension(parseInt(event.target.value));
+  };
+
   return (
     <div className="App">
-      <Game dimension={30} lifespan={lifespan}></Game>
-      <label>
-        Lifespan{" "}
-        <input
-          type="range"
-          name="lifespan"
-          id="lifespan"
-          min={1}
-          max={300}
-          onChange={lifespanOnChange}
-        />
-      </label>
+      <div>
+        <label>
+          Lifespan{" "}
+          <input
+            type="range"
+            name="lifespan"
+            id="lifespan"
+            min={1}
+            max={300}
+            value={initialLifespan}
+            onChange={lifespanOnChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Dimension{" "}
+          <input
+            type="range"
+            name="dimension"
+            id="dimension"
+            min={4}
+            max={50}
+            // value={initialDimension}
+            onChange={dimensionOnChange}
+          />
+        </label>
+      </div>
+      <Game dimension={dimension} lifespan={lifespan}></Game>
     </div>
   );
 }
